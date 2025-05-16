bf2ded4a94329191220c848f11aa0d3fe3b73afa
Mohamed Seglaby <mohamedseglaby22@gmail.com>
2025-05-16 11:01:51 +0300

    Choie:initialized the backend

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+app.get('/',(req,res)=>{
+    res.send('Hello world!')
+})
+app.get('/about',(req,res)=>{
+    res.send('About Page')
+})
+app.listen(4000,()=> console.log("Server On!"));
