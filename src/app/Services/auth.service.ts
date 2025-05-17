754e00c15284cdf39073fada3ff2f7c3d52ed0b3
Mohamed Seglaby <mohamedseglaby22@gmail.com>
2025-05-17 12:49:56 +0300

    feat:auth Service Created and Register function done

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+interface User{
+  id:string,
+  username:string,
+  role:string
+}
+interface authResponse{
+  message:string,
+  token?:string,
+  user?:string
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private URL = 'https://areeb-back-2025-akig.vercel.app/api/auth'
+  private token = '49e5a5551eba19c2daa88574b38717f237b5a5a00d75fdba17f753440dde67695cb35d27567f352019a2eb30b7cd49b2a7898491c0668ee70b6553cb5cfb3b28'
+  constructor(private http:HttpClient) { }
+
+  register(username:string,password:string,role?:string):Observable<any>{
+    return this.http.post<any>(`${this.URL}/register`,{username,password,role})
+  }
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule,FormGroup,Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
-
+export class RegisterComponent implements OnInit{
+registerForm!:FormGroup;
+constructor(private fb:FormBuilder,private authServ:AuthService){}
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      username:['',Validators.required],
+      password:['',Validators.required],
+      role:['',Validators.required]
+    })
+  }
+  onSubmit(){
+    if(this.registerForm.valid){
+      const {username,password,role}=this.registerForm.value;
+      this.authServ.register(username,password,role).subscribe({
+        next:(res)=>{
+          console.log(res)
+        },
+        error:(err)=>{
+          console.log(err)
+        }
+      })
+      console.log("form Submited",this.registerForm.value)
+    }
+  }
 }
