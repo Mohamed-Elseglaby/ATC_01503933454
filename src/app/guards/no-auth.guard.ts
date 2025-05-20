db3cf4f5e76337a4f7d0b0a8ec1a1169927c02d4
Mohamed Seglaby <mohamedseglaby22@gmail.com>
2025-05-20 15:15:16 +0300

    fix:make the admin access only the dashboard

--- src/app/guards/no-auth.guard.ts
+++ src/app/guards/no-auth.guard.ts
@@ -7,8 +7,12 @@ import { AuthService } from '../Services/auth.service';
 export class noAuthGuard implements CanActivate{
   constructor(private router:Router,private authServ:AuthService){}
   canActivate(): boolean {
-  if(localStorage.getItem('JWT')){
+  if(localStorage.getItem('JWT')&& localStorage.getItem('role')==='user'){
       this.router.navigate(['/home'])
+    return false
+  }
+  if(localStorage.getItem('JWT')&& localStorage.getItem('role')==='admin'){
+      this.router.navigate(['/dashboard'])
     return false
   }
   return true
